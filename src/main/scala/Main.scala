811f916a6172b57cdba948d69441ad9f41429811
nirbhaygpsdl <nirbhay@simpledatalabs.com>
2018-01-21 15:00:07 +0530

    Bubble sort

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,10 @@
+import algos._
+
+
+/**
+  * Created by nirbhaygupta on 1/21/18.
+  */
+object Main extends App {
+  println("This will be the main class which will execute the entire project")
+  BubbleSort.sort(Array(3,2,4, 5 ,6 ,7,1, 3, 1))
+}
---
+++ src/main/scala/algos/BubbleSort.scala
@@ -0,0 +1,27 @@
+package algos
+/**
+  * Created by nirbhaygupta on 1/21/18.
+  */
+object BubbleSort {
+  def sort(input: Array[Int]) : Array[Int]  = {
+      input match {
+        case Array(_*) => {
+          val arrayLength: Int = input.length
+          var minValueIndex   : Int = arrayLength - 1
+          for (i <- 0 to arrayLength - 2) {
+            for (j <- arrayLength - 1 to i by -1) {
+              if (input(j) < input(minValueIndex)) {
+                minValueIndex = j
+              }
+            }
+            val interim = input(i)
+            input(i) = input(minValueIndex)
+            input(minValueIndex) = interim
+          }
+        }
+        case _         => println("Its not")
+      }
+    println(input.deep.mkString(" "))
+    input
+  }
+}
