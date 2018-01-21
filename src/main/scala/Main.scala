754d1691e7decf9e833eb9892c8dc3bc543793db
nirbhaygpsdl <nirbhaywebdev@gmail.com>
2018-01-21 16:35:35 +0530

    Inserstion sort

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,5 +6,5 @@ import algos._
   */
 object Main extends App {
   println("This will be the main class which will execute the entire project")
-  BubbleSort.sort(Array(3,2,4, 5 ,6 ,7,1, 3, 1))
+  InsertionSort.sort(Array(3,2,4, 5 ,6 ,7,1, 3, 1))
 }
---
+++ src/main/scala/algos/InsertionSort.scala
@@ -0,0 +1,25 @@
+package algos
+
+/**
+  * Created by nirbhaygupta on 1/21/18.
+  */
+object InsertionSort {
+
+  def sort(input: Array[Int]): Array[Int] = {
+    val length = input.length
+    for (i <- 1 to length-1) {
+      var sortIndex = i;
+      while(sortIndex > 0) {
+        if(input(sortIndex) < input(sortIndex - 1)) {
+          val swap = input(sortIndex);
+          input(sortIndex) = input(sortIndex - 1)
+          input(sortIndex - 1) = swap
+        }
+        println(input.deep.mkString(" "))
+        sortIndex = sortIndex - 1
+      }
+    }
+    input
+  }
+
+}
