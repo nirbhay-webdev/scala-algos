c66d01b4b04a69172a53ad3c3d3eb270271973cb
nirbhaygpsdl <nirbhaywebdev@gmail.com>
2018-01-21 19:14:44 +0530

    Selection Sort

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,6 +6,6 @@ import algos._
   */
 object Main extends App {
   println("This will be the main class which will execute the entire project")
-  val output = MergeSort.sort(Array(3,2,4, 5 ,6 ,7,1, 3, 1))
+  val output = SelectionSort.sort(Array(3,2,4, 5 ,6 ,7,1, 3, 1))
   println(output.deep.mkString(" "))
 }
---
+++ src/main/scala/algos/SelectionSort.scala
@@ -0,0 +1,35 @@
+package algos
+
+/**
+  * Created by nirbhaygupta on 1/21/18.
+  */
+object SelectionSort {
+  def sort(input: Array[Int]): Array[Int] = {
+    var counter = 0;
+    var output = input
+    while(counter < input.length) {
+      val minIndex = min(input, counter)
+      output = replace(output,counter, minIndex)
+      counter = counter + 1
+    }
+    output
+  }
+
+  private def replace(arr: Array[Int], left: Int, right: Int): Array[Int] = {
+    val swap = arr(left)
+    arr(left) = arr(right)
+    arr(right) = swap
+    arr
+  }
+
+  private def min(arr: Array[Int], startFrom: Int): Int = {
+    var minIndex = startFrom;
+    for(i <- startFrom until arr.length) {
+      if(arr(i) < arr(minIndex)) {
+        minIndex = i
+      }
+    }
+    minIndex
+  }
+
+}
