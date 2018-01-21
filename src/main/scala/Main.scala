ffaf8a92765b992f9d308f04ab5f687d22043535
nirbhaygpsdl <nirbhaywebdev@gmail.com>
2018-01-21 18:40:56 +0530

    Merge sort

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,5 +6,6 @@ import algos._
   */
 object Main extends App {
   println("This will be the main class which will execute the entire project")
-  InsertionSort.sort(Array(3,2,4, 5 ,6 ,7,1, 3, 1))
+  val output = MergeSort.sort(Array(3,2,4, 5 ,6 ,7,1, 3, 1))
+  println(output.deep.mkString(" "))
 }
